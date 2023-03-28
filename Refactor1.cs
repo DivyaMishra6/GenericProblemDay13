6e409c1a0b72ad52efd1d2fe2528bdf914aec9de
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-29 02:18:12 +0530

    Refactor a Code

--- Refactor1.cs
+++ Refactor1.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace GenericsDemo
 {
-    internal class Refactor1
+    public class Refactor1
     {
+        public static T MaximumNum<T>(T FirstValue, T SecondValue, T ThirdValue) where T : IComparable
+        {
+            if (FirstValue.CompareTo(SecondValue) > 0 && FirstValue.CompareTo(ThirdValue) > 0 ||
+               FirstValue.CompareTo(SecondValue) >= 0 && FirstValue.CompareTo(ThirdValue) > 0 ||
+               FirstValue.CompareTo(SecondValue) > 0 && FirstValue.CompareTo(ThirdValue) > 0)
+            { return FirstValue; }
+
+            if (SecondValue.CompareTo(FirstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0 ||
+              SecondValue.CompareTo(FirstValue) >= 0 && SecondValue.CompareTo(ThirdValue) > 0 ||
+               SecondValue.CompareTo(FirstValue) > 0 && SecondValue.CompareTo(ThirdValue) > 0)
+            { return SecondValue; }
+
+            if (ThirdValue.CompareTo(FirstValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0 ||
+              ThirdValue.CompareTo(FirstValue) >= 0 && ThirdValue.CompareTo(SecondValue) > 0 ||
+              ThirdValue.CompareTo(FirstValue) > 0 && ThirdValue.CompareTo(SecondValue) > 0)
+            { return ThirdValue; }
+
+
+            return FirstValue;
+        }
     }
 }
